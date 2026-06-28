09bd347afbf2629d6468ef5f4b93a8323d1e5ec4
Lord Zurra <>
2026-06-28 22:23:05 +0300

    Updater LXR-CE to 5.8

--- Source/LXR/Private/Widget/ULXRDebugFeature.cpp
+++ Source/LXR/Private/Widget/ULXRDebugFeature.cpp
@@ -13,115 +13,115 @@
 
 AActor* UULXRDebugFeature::GetMyDebugActor() const
 {
-	return DebugEntryWidget.Get()->GetMyDebugActor();
+    return DebugEntryWidget.Get()->GetMyDebugActor();
 }
 
 UActorComponent* UULXRDebugFeature::GetMyComponent(TSubclassOf<UActorComponent> FeatureComponent) const
 {
-	UActorComponent* ComponentToReturn;
-	if (MyDebugFeature == ELXRDebugFeature::Sockets && FeatureComponent.Get() == ULXRFluxLightDetectorComponent::StaticClass())
-	{
-		ComponentToReturn = DebugEntryWidget->MyEntryObject->GetMyComponent(ELXRDebugFeature::Capture);
-	}
-	else
-	{
-		ComponentToReturn = DebugEntryWidget->MyEntryObject->GetMyComponent(MyDebugFeature);
-	}
-	return ComponentToReturn;
+    UActorComponent* ComponentToReturn;
+    if (MyDebugFeature == ELXRDebugFeature::Sockets && FeatureComponent.Get() == ULXRFluxLightDetectorComponent::StaticClass())
+    {
+        ComponentToReturn = DebugEntryWidget->MyEntryObject->GetMyComponent(ELXRDebugFeature::Capture);
+    }
+    else
+    {
+        ComponentToReturn = DebugEntryWidget->MyEntryObject->GetMyComponent(MyDebugFeature);
+    }
+    return ComponentToReturn;
 }
 
 bool UULXRDebugFeature::IsMyFeatureEnabled() const
 {
-	auto DebugFeatures = GetMyDebugFeatures();
+    auto DebugFeatures = GetMyDebugFeatures();
 
-	bool DebugFeatureEnabled = false;
-	bool IsComponentAvailable = false;
-	bool FeatureEnabledInComponent = true;
-	UActorComponent* MyFeatureComponent = nullptr;
-	switch (MyDebugFeature)
-	{
-	case ELXRDebugFeature::Direct:
-		DebugFeatureEnabled = DebugFeatures->bEnableDirect || DebugFeatures->bAutoDetect;
-		MyFeatureComponent = DebugEntryWidget->MyEntryObject->GetMyComponent(ELXRDebugFeature::Direct);
-		break;
-	case ELXRDebugFeature::Sense:
-		DebugFeatureEnabled = DebugFeatures->bEnableSense || DebugFeatures->bAutoDetect;
-		MyFeatureComponent = DebugEntryWidget->MyEntryObject->GetMyComponent(ELXRDebugFeature::Sense);
-		break;
+    bool DebugFeatureEnabled = false;
+    bool IsComponentAvailable = false;
+    bool FeatureEnabledInComponent = true;
+    UActorComponent* MyFeatureComponent = nullptr;
+    switch (MyDebugFeature)
+    {
+    case ELXRDebugFeature::Direct:
+        DebugFeatureEnabled = DebugFeatures->bEnableDirect || DebugFeatures->bAutoDetect;
+        MyFeatureComponent = DebugEntryWidget->MyEntryObject->GetMyComponent(ELXRDebugFeature::Direct);
+        break;
+    case ELXRDebugFeature::Sense:
+        DebugFeatureEnabled = DebugFeatures->bEnableSense || DebugFeatures->bAutoDetect;
+        MyFeatureComponent = DebugEntryWidget->MyEntryObject->GetMyComponent(ELXRDebugFeature::Sense);
+        break;
 
-	case ELXRDebugFeature::Capture:
-		DebugFeatureEnabled = DebugFeatures->bEnableCapture || DebugFeatures->bAutoDetect;
-		MyFeatureComponent = DebugEntryWidget->MyEntryObject->GetMyComponent(ELXRDebugFeature::Capture);
-		break;
-	case ELXRDebugFeature::Sockets:
-		DebugFeatureEnabled = DebugFeatures->bEnableSockets || DebugFeatures->bAutoDetect;
-		MyFeatureComponent = DebugEntryWidget->MyEntryObject->GetMyComponent(ELXRDebugFeature::Sockets);
-		if (MyFeatureComponent)
-		{
-			if (auto DetectionComponent = Cast<ULXRDetectionComponent>(MyFeatureComponent))
-			{
-				FeatureEnabledInComponent = DetectionComponent->VisibilityTargetType == ETraceTarget::Sockets;
-			}
-		}
-		break;
+    case ELXRDebugFeature::Capture:
+        DebugFeatureEnabled = DebugFeatures->bEnableCapture || DebugFeatures->bAutoDetect;
+        MyFeatureComponent = DebugEntryWidget->MyEntryObject->GetMyComponent(ELXRDebugFeature::Capture);
+        break;
+    case ELXRDebugFeature::Sockets:
+        DebugFeatureEnabled = DebugFeatures->bEnableSockets || DebugFeatures->bAutoDetect;
+        MyFeatureComponent = DebugEntryWidget->MyEntryObject->GetMyComponent(ELXRDebugFeature::Sockets);
+        if (MyFeatureComponent)
+        {
+            if (auto DetectionComponent = Cast<ULXRDetectionComponent>(MyFeatureComponent))
+            {
+                FeatureEnabledInComponent = DetectionComponent->VisibilityTargetType == ETraceTarget::Sockets;
+            }
+        }
+        break;
 
-	case ELXRDebugFeature::None:
-		return false;
-	default: ;
-	}
-	IsComponentAvailable = MyFeatureComponent != nullptr;
+    case ELXRDebugFeature::None:
+        return false;
+    default: ;
+    }
+    IsComponentAvailable = MyFeatureComponent != nullptr;
 
-	return DebugFeatureEnabled && IsComponentAvailable && FeatureEnabledInComponent;
+    return DebugFeatureEnabled && IsComponentAvailable && FeatureEnabledInComponent;
 }
 
 TSubclassOf<UActorComponent> UULXRDebugFeature::GetMyDebugComponentClass() const
 {
-	switch (MyDebugFeature)
-	{
-	case ELXRDebugFeature::Direct:
-	case ELXRDebugFeature::Sockets:
-		return ULXRDetectionComponent::StaticClass();
-	case ELXRDebugFeature::Sense:
-		return ULXRLightSenseComponent::StaticClass();
-	case ELXRDebugFeature::Capture:
-		return ULXRFluxLightDetectorComponent::StaticClass();
+    switch (MyDebugFeature)
+    {
+    case ELXRDebugFeature::Direct:
+    case ELXRDebugFeature::Sockets:
+        return ULXRDetectionComponent::StaticClass();
+    case ELXRDebugFeature::Sense:
+        return ULXRLightSenseComponent::StaticClass();
+    case ELXRDebugFeature::Capture:
+        return ULXRFluxLightDetectorComponent::StaticClass();
 
-	case ELXRDebugFeature::None:
-		break;
-	}
-	return nullptr;
+    case ELXRDebugFeature::None:
+        break;
+    }
+    return nullptr;
 }
 
 
 void UULXRDebugFeature::PrepareWidget()
 {
-	SetMyComponent(GetMyDebugActor()->GetComponentByClass(GetMyDebugComponentClass()));
-	DebugFeaturesChanged();
-	BP_WidgetReady();
+    SetMyComponent(GetMyDebugActor()->GetComponentByClass(GetMyDebugComponentClass()));
+    DebugFeaturesChanged();
+    BP_WidgetReady();
 }
 
 void UULXRDebugFeature::DebugFeaturesChanged()
 {
-	SetVisibility(IsMyFeatureEnabled() ? ESlateVisibility::HitTestInvisible : ESlateVisibility::Collapsed);
+    SetVisibility(IsMyFeatureEnabled() ? ESlateVisibility::HitTestInvisible : ESlateVisibility::Collapsed);
 }
 
 void UULXRDebugFeature::NativeCustomTick()
 {
-	if (IsVisible())
-		CustomTick();
+    if (IsVisible())
+        CustomTick();
 }
 
 void UULXRDebugFeature::SetMyComponent(UActorComponent* InComponent)
 {
-	DebugEntryWidget->MyEntryObject->SetComponent(MyDebugFeature, InComponent);
+    DebugEntryWidget->MyEntryObject->SetComponent(MyDebugFeature, InComponent);
 }
 
 void UULXRDebugFeature::SetDebugEntryWidget(UULXRDebugEntry& InEntryWidget)
 {
-	DebugEntryWidget = InEntryWidget;
+    DebugEntryWidget = &InEntryWidget;
 }
 
 TSharedPtr<FLXRDebugFeatures> UULXRDebugFeature::GetMyDebugFeatures() const
 {
-	return DebugEntryWidget->MyEntryObject->MyDebugFeatures;
+    return DebugEntryWidget->MyEntryObject->MyDebugFeatures;
 }
